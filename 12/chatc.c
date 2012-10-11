4e0e305d03ad219ae9dd2c5d02adf289dec117a5
student <csa472f1@lab67.net.cecs.csulb.edu>
2012-10-10 18:58:16 -0700

    ?

---
+++ 12/chatc.c
@@ -0,0 +1,98 @@
+/*
+ * A. Shawn Bandy F1
+ * CECS 472
+ * Assignment 12 - Chat Client
+ * 10/10/2012
+ */
+
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/time.h>
+
+int     TCPecho(const char *host, const char *service);
+#include "connectTCP.c"
+#include "get_port.c"
+
+/* handled by the include
+ * int     errexit(const char *format, ...);
+ * int     connectTCP(const char *host, const char *service);
+ */
+#define LINELEN         128
+#define BUFSIZE		4096
+int main(int argc, char *argv[])
+{
+        char    *host = "localhost";    /* host to use if none supplied */
+        char    *service = get_port();      /* default service name         */
+	struct timeval timeout;
+	int ssock;
+	fd_set rfds;
+	fd_set afds;
+	int nfds;
+	int cc;
+
+        switch (argc) {
+        case 1:
+                break;
+        case 3:
+                service = argv[2];
+                /* FALL THROUGH */
+        case 2:
+                host = argv[1];
+                break;
+        default:
+                fprintf(stderr, "usage: TCPecho [host [port]]\n");
+                exit(1);
+        }
+        signal(SIGPIPE, SIG_IGN);
+
+	ssock = connectTCP(host,service);
+        nfds = getdtablesize();
+        
+	FD_ZERO(&afds);
+        FD_SET(0, &afds);
+	FD_SET(ssock,&afds);
+
+
+        while (1) {
+		char buf[BUFSIZE] = {};
+ 		timeout.tv_sec = 5;
+		timeout.tv_usec = 0;
+ 		memcpy(&rfds, &afds, sizeof(rfds));
+
+                if (select(nfds, &rfds, (fd_set *)0, (fd_set *)0,
+                                &timeout) < 0)
+                        errexit("select: %s\n", strerror(errno));
+		if(timeout.tv_sec == 0 && timeout.tv_usec ==0 ) {
+			printf("Are you there?\n");
+		} else {
+			if(FD_ISSET(0,&rfds)) { 
+				cc = read(0,buf,sizeof(buf));
+				if(cc <= 0) {
+					FD_CLR(ssock,&afds);
+					(void) close(ssock);
+					printf("Goodbye.\n");
+					exit(0);
+				} else {
+					send(ssock,buf,cc,0);
+				}
+			}
+			if(FD_ISSET(ssock,&rfds)) {
+				cc = recv(ssock,buf,sizeof(buf),0);
+				if(cc <= 0) {
+					FD_CLR(ssock,&afds);
+					(void) close(ssock);
+					printf("Goodbye.\n");
+					exit(0);
+				} else {
+					printf("%s",buf);
+				}
+			}
+		}
+	}					
+ 	exit(0);	
+}
